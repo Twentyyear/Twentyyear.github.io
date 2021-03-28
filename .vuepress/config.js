e34dc55987d86c21116fac54569b22012b80edb1
Twentyyear <827258516@qq.com>
2021-03-28 19:29:41 +0800

    push

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,50 +1,54 @@
 module.exports = {
-    "title": "Twentyyear的个人空间",
-    "description": "myblog",
-    "dest": "public",
-    "head": [
+    title: "Twentyyear的博客",
+    description: "优秀不够，你是否无可替代",
+    dest: "public",
+    head: [
         [
             "link",
             {
-                "rel": "icon",
-                "href": "/favicon.ico"
+                rel: "icon",
+                href: "/favicon.ico"
             }
         ],
-        [
-            "meta",
+        ["meta",
             {
-                "name": "viewport",
-                "content": "width=device-width,initial-scale=1,user-scalable=no"
+                name: "viewport",
+                content: "width=device-width,initial-scale=1,user-scalable=no"
             }
         ]
     ],
-    "theme": "reco",
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+    theme: "reco",
     //主题配置文件
-    "themeConfig": {
+    themeConfig: {
         //导航栏配置
-        "nav": [
+        nav: [
             {
-                "text": "主页",
-                "link": "/",
-                "icon": "reco-home"
+                text: "主页",
+                link: "/",
+                icon: "reco-home"
             },
             {
-                "text": "时间轴",
-                "link": "/timeline/",
-                "icon": "reco-date"
+                text: "时间轴",
+                link: "/timeline/",
+                icon: "reco-date"
             },
             {
-                "text": "系列教程",
-                "icon": "reco-message",
-                "items": [
+                text: "系列教程",
+                icon: "reco-message",
+                items: [
                     {
-                        "text": "vuepress-reco",
-                        "link": "/docs/theme-reco/"
+                        text: "vuepress-reco",
+                        link: "/docs/theme-reco/"
                     }
                 ]
             },
         ],
-        "sidebar": {
+        sidebar: {
             "/docs/theme-reco/": [
                 "",
                 "theme",
@@ -52,43 +56,47 @@ module.exports = {
                 "api"
             ],
         },
-        "type": "blog",
-        //风格
-        "blogConfig": {
-            "category": {
-                "location": 2,
-                "text": "分类"
-            },
-            "tag": {
-                "location": 3,
-                "text": "标签"
-            }
-        },
-        "友情链接": [
+        type: "blog",
+        friendLink: [
             {
-                "title": "1090影视",
-                "desc": "高清在线视频网站",
-                "email": "",
-                "link": "http://1090ys2.com/"
+                title: '1090影视',
+                desc: '一个高清在线视频站点',
+                link: 'http://1090ys2.com/'
+            },
+            {
+                title: '高清电台',
+                desc: '一个高清影视资源下载站点',
+                link: 'https://gaoqing.fm/'
             },
         ],
-        "logo": "/logo.png",
+        //博客配置
+        blogConfig: {
+            category: {
+                location: 2,// 在导航栏菜单中所占的位置，默认2
+                text: "分类"
+            },
+            tag: {
+                location: 3,// 在导航栏菜单中所占的位置，默认3
+                text: "标签"
+            },
+        },
+        logo: "/logo.png",
         //当前项目是否要开启搜索栏
-        "search": true,
-        "searchMaxSuggestions": 10,
-        "lastUpdated": "Last Updated",
-        "author": "Twentyyear",
-        "authorAvatar": "/avatar.png",
-        "record": "版号",
-        "startYear": "2021"
+        search: true,
+        searchMaxSuggestions: 10,
+        lastUpdated: "Last Updated",
+        author: "Twentyyear",
+        authorAvatar: "/logo.png",
+        record: "版号",
+        startYear: "2021"
     },
     //代码的行数是否要显示
-    "markdown": {
-        "lineNumbers": true
+    markdown: {
+        lineNumbers: true
     },
     //插件
-    "plugins": {"vuepress-plugin-auto-sidebar": {}},
-    "plugins": [
+    plugins: {"vuepress-plugin-auto-sidebar": {}},
+    plugins: [
         [
             "@vuepress-reco/vuepress-plugin-kan-ban-niang",
             {
